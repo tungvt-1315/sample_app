6e35ae99ae29b0368d4a42e30b71d1b2199d2573
Vu Thanh Tung <vu.thanh.tung-b@sun-asterisk.com>
2023-07-05 14:09:37 +0700

    Chapter 3, 4, 5: Static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
-module ApplicationHelper
+module ApplicationHelper 
+    def full_title page_title = ""
+        base_title = "Ruby on Rails Tutorials Sample App"
+        page_title.empty? ? base_title : "#{page_title} | #{base_title}" 
+    end
 end
