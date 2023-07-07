d9b93c1e53c377ee6b9a1bfa182e8f15453858d3
Vu Thanh Tung <vu.thanh.tung-b@sun-asterisk.com>
2023-07-07 15:02:26 +0700

    Chapter7: Sign-up -fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  resources :products
-  root 'static_pages#home'
+  root "static_pages#home"
 
-  get 'static_pages/home'
-  get 'static_pages/help'
+  get "static_pages/home"
+  get "static_pages/help"
 
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
