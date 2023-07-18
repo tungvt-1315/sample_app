9088f814d2c6a04cad8ff9e02cb7ecdd69fdb905
Vu Thanh Tung <vu.thanh.tung-b@sun-asterisk.com>
2023-07-18 16:31:51 +0700

    Chapter 12: Password reset

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,14 +1,25 @@
 class AccountActivationsController < ApplicationController
+  before_action :find_user, only: :edit
+
   def edit
-    user = User.find_by email: params[:email]
-    if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      user.activate
-      log_in user
+    if @user && !@user.activated? && @user.authenticated?(:activation,
+                                                          params[:id])
+      @user.activate
+      log_in @user
       flash[:success] = t "controller.users.activate.success"
-      redirect_to user
+      redirect_to @user
     else
       flash[:danger] = t "controller.users.activate.fail"
       redirect_to root_path
     end
+  end
+
+  private
+  def find_user
+    @user = User.find_by email: params[:email]
+    return if @user
+
+    flash[:warning] = t "controller.users.show.not_found"
+    redirect_to root_path
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_17_041224) do
+ActiveRecord::Schema.define(version: 2023_07_18_080128) do
 
   create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2023_07_17_041224) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
 end
